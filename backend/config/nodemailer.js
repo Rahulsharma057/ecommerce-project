59b3d7b5e9125888dbfdf3105b1465124beb6104
Vijay Singh <vijay.singh@staqo.com>
2026-07-14 17:37:20 +0530

    form validation and email isuee

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -5,12 +5,15 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
 
+  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 
   connectionTimeout: 10000,
+  greetingTimeout: 10000,
   socketTimeout: 10000,
 });
 
