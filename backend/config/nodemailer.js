f82f18a6f4d9bfb6c9cd131042569d88e6ae1857
Vijay Singh <vijay.singh@staqo.com>
2026-07-14 20:33:17 +0530

    new try ek or

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,24 +1,16 @@
 const nodemailer = require("nodemailer");
-
+console.log("SMTP USER:", process.env.SMTP_USER);
+console.log("SMTP PASS:", process.env.SMTP_PASS?.slice(0,10));
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  port: process.env.SMTP_PORT,
   secure: false,
-
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
 
-
-transporter.verify((err) => {
-  if (err) {
-    console.log("SMTP ERROR:", err);
-  } else {
-    console.log("SMTP READY");
-  }
+transporter.verify((err, success) => {
+  console.log(err || success);
 });
-
-
-module.exports = transporter;
--- backend/utils/sendOtpEmail.js
+++ backend/utils/sendOtpEmail.js
@@ -1,12 +1,12 @@
 const transporter = require("../config/nodemailer");
 
 const sendOtpEmail = async (email, otp) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Your OTP Code",
-    text: `Your OTP is ${otp}`,
-  });
+ await transporter.sendMail({
+  from: `"VELOURA" <${process.env.EMAIL_USER}>`, // yahan Gmail address rakh sakte ho display ke liye
+  to: email,
+  subject: "Your OTP Code",
+  text: `Your OTP is ${otp}`,
+});
 };
 
 module.exports = sendOtpEmail;
