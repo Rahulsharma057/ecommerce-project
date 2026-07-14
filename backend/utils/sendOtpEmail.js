fc3776499e6d5f34f43f3f2f1d628942f834d08e
Vijay Singh <vijay.singh@staqo.com>
2026-07-14 16:32:20 +0530

    email issue fix 2

--- backend/utils/sendOtpEmail.js
+++ backend/utils/sendOtpEmail.js
@@ -1,15 +1,4 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4, // Force IPv4
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const transporter = require("../config/nodemailer");
 
 const sendOtpEmail = async (email, otp) => {
   await transporter.sendMail({
